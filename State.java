ac4b384fa086a4b3793172c188fa17d40be9c3d2
sarah_deschamps <deschamps.sias@gmail.com>
2025-09-23 00:03:29 -0500

    Implement Position and State

--- State.java
+++ State.java
@@ -1,3 +1,11 @@
 public class State {
-
+  private Position objPos;
+  private Position[] agentPos;
+  private int[] agentR;
+  
+  public State(Position objPos, Position[] agentPos, int[] agentR) {
+    this.objPos = objPos;
+    this.agentPos = agentPos;
+    this.agentR = agentR;
+  }
 }
