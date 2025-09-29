704b6b3023b82d9b08fed9718b1b316a190c58a5
sarah_deschamps <deschamps.sias@gmail.com>
2025-09-28 19:03:25 -0500

    Implement Agent

--- Agent.java
+++ Agent.java
@@ -1,3 +1,41 @@
-public class Agent {
+import java.util.Random;
 
+public class Agent {
+  private int id;
+  private Position pos;
+  private float[] probs;
+  private int rval;
+
+  public Agent(int id, Position pos, float[] probs) {
+    this.id = id;
+    this.pos = pos;
+    this.probs = probs;
+    this.rval = 0;
+  }
+
+  public int pickAction() {
+    Random rand = new Random();
+    double action = rand.nextDouble();
+    if (action < probs[0]) {
+      return 0;
+    } else if (action < probs[1]) {
+      return 1;
+    } else if (action < probs[2]) {
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+
+  public void addPunishment(int n) {
+    this.rval -= n;
+  }
+
+  public void addReward(int n) {
+    this.rval += n;
+  }
+
+  public int getR() {
+    return this.rval;
+  }
 }
